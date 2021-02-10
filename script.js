b9524a6c67113ca92d3574b89264f849c73ab9c8
Kuba \Pi <kubapilat80@gmail.com>
2021-02-10 08:32:52 +0000

    petla for, 2D

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var tab = [1, 2, 3, 4, 5], i, suma=0;
+var m = [
+	[0,1,2],
+	[3,4,5],
+	[6,7,8]
+];
+var i,k;
 
-for (i = 0; i < tab.length; i++) {
-	if(i>2){
-		suma= suma + tab[i];
-		document.write(suma, "|")
+for (i=0; i<m.length; i++){
+	for(k=0; k<m[i].length; k++){
+		document.write(m[i][k]=0)
 	}
 }
-
-// for (i = 0; i < tab.length; i++) {
-// 	document.write(suma, "|")
-// }
