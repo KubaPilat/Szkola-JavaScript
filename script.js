302a5842cb1305621a0924aec619293e98e82b9b
Kuba \Pi <kubapilat80@gmail.com>
2021-02-17 08:41:57 +0000

    SZACOWNICA DO ZROBIENIA!!!

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-var wektor = [];
-var i;
-// dodanie randomowe
-for (i = 0; i < 10; i++) {
-	wektor[i] = Math.floor(Math.random() * 11);
+// var wektor = [];
+// var i;
+// // szachownica I liniowa
+// for (i = 0; i < 10; i++) {
+// 	wektor[i]=i%2
+// }
+// document.write(wektor, '<br>')
+
+// tablica dwuwymiarowa - macierz
+var wektor = [ [1, 2, 3], 
+				[4, 5, 6], 
+				[7, 8, 9]  ]; 
+var k;
+var w;			//	[w0, w1, w2]
+
+for(w = 0; w < wektor.length; w++) {
+	for(k = 0; k < wektor[w].length; k++) {
+		document.write(wektor[w][k] + " "); 
+	}
+	document.write("<br>");
 }
 
-document.write(wektor + '<br>');
-// if wiekszy niz np.5
-for (i = 0; i < 10; i++) {
-	if (wektor[i] > 5) {
-		document.write(wektor[i] + '|')
+for(w = 0; w < wektor.length; w++) {
+	for(k = 0; k < wektor[w].length; k++) {
+		wektor[w][k] = 0; // wektor[w][k]
 	}
 }
 
-document.write('<br>');
-// sortowanie
-var min = wektor[0];
-for (i = 0; i < 10; i++) {
-	if (wektor[i] < min) {
-		min = wektor[i];
-	}
-}
-
-document.write(min,'<br>')
-//ciag fibonacciego
-wektor[0]=0;
-wektor[1]=1;
-for (i = 2; i < 20; i++) {
-	wektor[i] = wektor[i-1] + wektor[i-2];
-}
-document.write(wektor)
+// szybki podglÄd
+document.write("</br>" + wektor + "</br></br>");
