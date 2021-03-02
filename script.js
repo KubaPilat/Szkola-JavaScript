a97d05aec8da7170b62abd82f3f751189cb89c05
Kuba \Pi <kubapilat80@gmail.com>
2021-03-02 20:18:47 +0000

    v1

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
 var lp = 1;
 
-// while (lp < 11) {
-// 	document.write('<tr> <td>' + lp + '</td> <td>while</td> </tr>')
-// 	lp++
-// }
+while (lp < 11) {
+	document.write('<tr> <td>' + lp + '</td> <td>')
+	if (lp % 2 == 0) {
+		document.write('parzysta')
+	} else {
+		document.write('nieparzysta')
+	}
+	document.write('</td> </tr>')
+	lp++
+}
 
-// for (lp = 1; lp < 11; lp++) {
-// 	document.write('<tr> <td>' + lp + '</td> <td>for</td> </tr>')
-// }
+document.write('<tr> <td> PETLA FOR</td> <tr>')
+
+for (lp = 1; lp < 11; lp++) {
+ 	document.write('<tr> <td>' + lp + '</td> <td>')
+	if (lp % 2 == 0) {
+		document.write('parzysta')
+	} else {
+		document.write('nieparzysta')
+	}
+	document.write('</td> </tr>')
+}
+
+document.write('<tr> <td> PETLA DO</td> <tr>')
+
+var lp = 1;
 
 do {
-	document.write('<tr> <td>' + lp + '</td> <td>do while</td> </tr>')
+	document.write('<tr> <td>' + lp + '</td> <td>')
+	if (lp % 2 == 0) {
+		document.write('parzysta')
+	} else {
+		document.write('nieparzysta')
+	}
+	document.write('</td> </tr>')
 	lp++
-} while (lp < 11)
+} while (lp < 11) 
+
+document.write('<br>');
+
+var wektor = [];
+ var i;
+ for (i = 0; i < 10; i++) {
+ 	wektor[i] = Math.floor(Math.random() * 11);
+ }
+ document.write(wektor)
+
+for (i=0; i<10; i++){
+	document.write('<tr> <td>'+i+'</td><td>'+wektor[i]+'</td></tr>')
+}
