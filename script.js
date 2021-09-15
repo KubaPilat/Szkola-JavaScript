42b872938fc3b0b6db959ff2f0a2ba0bb952a4dc
klasa nr 1 <kubapilat80@gmail.com>
2021-09-15 09:36:53 +0200

    poprawki dodanie godziny

---
+++ script.js
@@ -0,0 +1,20 @@
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = dd + '/' + mm + '/' + yyyy;
+document.write(today);
+
+var span = document.getElementById('span');
+
+function time() {
+    var d = new Date();
+    var s = d.getSeconds();
+    var m = d.getMinutes();
+    var h = d.getHours();
+    span.textContent =
+        ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
+}
+
+setInterval(time, 1000);
