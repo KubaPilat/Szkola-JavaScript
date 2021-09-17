8a14b79d9c355eea0f955ba43281dfa9139693c9
klasa nr 1 <kubapilat80@gmail.com>
2021-09-17 12:26:15 +0200

    pierdoly

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
+let a = document.getElementById("1");
+document.removeChild(a);
 
-today = dd + '/' + mm + '/' + yyyy;
-document.write(today);
 
-var span = document.getElementById('span');
 
-function time() {
-    var d = new Date();
-    var s = d.getSeconds();
-    var m = d.getMinutes();
-    var h = d.getHours();
-    span.textContent =
-        ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
-}
 
-setInterval(time, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var span = document.getElementById('span');
+// function time() {
+//     var d = new Date();
+//     var s = d.getSeconds();
+//     var m = d.getMinutes();
+//     var h = d.getHours();
+//     span.textContent =
+//         ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
+// }
+// setInterval(time, 1000);
