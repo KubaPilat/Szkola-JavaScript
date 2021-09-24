07d52433df996498abe38d3d31f6b62094949546
klasa nr 1 <kubapilat80@gmail.com>
2021-09-24 10:55:43 +0200

    pierdoly

--- script.js
+++ script.js
@@ -1,52 +1,6 @@
-let a = document.getElementById("1");
-document.removeChild(a);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var span = document.getElementById('span');
-// function time() {
-//     var d = new Date();
-//     var s = d.getSeconds();
-//     var m = d.getMinutes();
-//     var h = d.getHours();
-//     span.textContent =
-//         ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
-// }
-// setInterval(time, 1000);
+var x=48;
+var y='1';
+document.write(x);
+document.write(typeof(x)+" <br>");
+document.write(y);
+document.write(typeof(y)+"<br>");
