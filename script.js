5d8cb8c55c3fa7d1e2b8b133c086bed4518c5fc3
sala 1 <kuba>
2021-10-27 09:33:01 +0200

    zadanie niedokonczone

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-var a = parseInt(document.getElementById("a").value);
-document.write(typeof(a));
+
+    var imie = document.getElementById("imie").checked;
+
+    function WypiszImie() {
+        var a = prompt("Imię?");
+        document.getElementById("demo").innerHTML="Witaj " + a;
+    }
+
+    function WypiszDate() {
+        var data = Date();
+        document.getElementById("demo").innerHTML = data;
+    }
+
+    function WypiszLiczba() {
+        var numer = parseInt(document.getElementById("numer").value);
+        document.getElementById("demo").innerHTML = numer+6;
+    }
+
+
+    WypiszLiczba()
+
+
